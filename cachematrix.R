4e57c3da31b152a6c9c54d305651e7d906e37c79
ALI <ali.inam03@gmail.com>
2015-12-24 13:09:11 +0500

    Done

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,43 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#set the value of the matrix
+#get the value of the matrix 
+#set the value of the inverse of the matrix 
+#get the value of the inverse of the matrix
 
+makeCacheMatrix <- function(x = matrix()) {
+    i <- NULL;
+    set <- function(y){
+      x <<- y
+      i <<- NULL
+    }
+    get <- function()x
+    setinverse <- function(inv)i <<- inv
+    getinverse <- function()i
+    
+    list(
+      set = set,
+      get = get,
+      setinverse = setinverse,
+      getinverse = getinverse
+    )
 }
 
 
 ## Write a short comment describing this function
+#This function first checks if the same matrix has already been inverted or not. If yes then return the value from the  cache other wise 
+#you need to computer it again
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)){
+          message("Found Cache Data")
+          return (i)
+        }
+        m <- x$get()
+        i <- solve(m,...)
+        x$setinverse(i)
+        i
 }
